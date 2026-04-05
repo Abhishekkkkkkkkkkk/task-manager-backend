5ff7e6239f22a94e0b07b2b177344c113fbff9c2
Abhishek kumar <109682053+Abhishekkkkkkkkkkk@users.noreply.github.com>
2026-04-05 14:35:42 +0530

    prisma error fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -14,21 +14,3 @@ export const prisma =
 if (env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
-
-
-
-
-
-
-
-// import { PrismaClient } from '@prisma/client';
-
-// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
-//   });
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
